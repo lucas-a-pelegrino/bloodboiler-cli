6fc66e1963f24a7c593afe2333a9945aa2c3262b
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2021-01-19 17:16:08 -0300

    build: first commit

---
+++ docs/commands.md
@@ -0,0 +1,3 @@
+# Command Reference for bloodboiler-cli
+
+TODO: Add your command reference here
---
+++ src/commands/bloodboiler-cli.js
@@ -0,0 +1,11 @@
+
+const command = {
+  name: 'bloodboiler-cli',
+  run: async toolbox => {
+    const { print } = toolbox
+
+    print.info('Welcome to your CLI')
+  }
+}
+
+module.exports = command
